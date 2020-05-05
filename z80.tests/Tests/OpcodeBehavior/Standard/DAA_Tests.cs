063997c873a0440764351293c21c586d5b83c046
Justin Unterreiner <justin.unterreiner@gmail.com>
2020-05-04 20:44:04 -0700

    Add test for DAA

---
+++ z80.tests/Tests/OpcodeBehavior/Standard/DAA_Tests.cs
@@ -0,0 +1,60 @@
+using Xunit;
+
+namespace JustinCredible.ZilogZ80.Tests
+{
+    public class DAA_Tests : BaseTest
+    {
+        // This covers the example from the 8080 Programmers Manual.
+        [Fact]
+        public void Test_DAA_DoubleCarry()
+        {
+            var rom = AssembleSource($@"
+                org 00h
+                DAA
+                HALT
+            ");
+
+            var registers = new CPURegisters()
+            {
+                A = 0x9B,
+            };
+
+            var flags = new ConditionFlags()
+            {
+                Zero = true,
+                Sign = true,
+                Parity = true,
+                Carry = false,
+                AuxCarry = false,
+                Subtract = true,
+            };
+
+            var initialState = new CPUConfig()
+            {
+                Registers = registers,
+                Flags = flags,
+            };
+
+            var state = Execute(rom, initialState);
+
+            // Ensure these flags were updated.
+            Assert.False(state.Flags.Zero);
+            Assert.False(state.Flags.Sign);
+            Assert.False(state.Flags.Parity);
+
+            // Both carry bits are set in this case.
+            Assert.True(state.Flags.Carry);
+            Assert.True(state.Flags.AuxCarry);
+
+            // Ensure this flag was unaffected.
+            Assert.True(state.Flags.Subtract);
+
+            // Verify accumulator value.
+            Assert.Equal(1, state.Registers.A);
+
+            Assert.Equal(2, state.Iterations);
+            Assert.Equal(4 + 4, state.Cycles);
+            Assert.Equal(0x01, state.ProgramCounter);
+        }
+    }
+}
